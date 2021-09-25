05c5bbca3be6d3000b41457a2935611fb2a097f6
Cristian Ortega <cristianortega12@gmail.com>
2021-09-25 09:50:16 -0500

    Actualizo Servlet Cliente y Proveedor

---
+++ src/main/java/Controlador/Cliente.java
@@ -0,0 +1,114 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+
+import Modelo.ClienteDAO;
+import Modelo.ClienteDTO;
+import Modelo.UsuarioDAO;
+import Modelo.UsuarioDTO;
+
+/**
+ * Servlet implementation class Cliente
+ */
+@WebServlet("/Cliente")
+public class Cliente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Cliente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ClienteDAO clientedao = new ClienteDAO();
+		//pregunto si el cliente hizo clic en el boton registrar
+		if(request.getParameter("registrar")!=null) {
+			String email,nombre,direccion,telefono;
+			long cedula;
+			cedula = Long.parseLong(request.getParameter("cedula_cliente"));
+			direccion = request.getParameter("direccion_cliente");
+			email = request.getParameter("email_cliente");
+			nombre = request.getParameter("nombre_cliente");
+			telefono = request.getParameter("telefono_cliente");
+			ClienteDTO clientedto = new ClienteDTO(cedula,direccion,email,nombre,telefono);
+			if(clientedao.InsertarCliente(clientedto)) {
+		      JOptionPane.showMessageDialog(null, "Cliente Registrado Exitosamente!");	
+              response.sendRedirect("Clientes.jsp?men=Cliente Registrado Exitosamente!");  		    
+		    }else {
+		      JOptionPane.showMessageDialog(null, "El Cliente no se Registro");	
+	          response.sendRedirect("Clientes.jsp?men=El Cliente no se Registro");
+		    }
+		}
+		
+		
+		if(request.getParameter("consultar")!=null){
+			String email,nombre,direccion,telefono;
+			long cedula;
+			cedula =Long.parseLong(request.getParameter("cedula_cliente2"));
+			ClienteDTO cli = clientedao.ConsultarCliente(cedula);
+			if(cli!=null) {
+				cedula= cli.getCedula_cliente();
+				direccion = cli.getDireccion_cliente();
+				email = cli.getEmail_cliente();
+				nombre =  cli.getNombre_cliente();
+				telefono = cli.getTelefono_cliente();
+				
+				//ahora enviar por la url con el response a Libros.jsp o la vista jsp
+				response.sendRedirect("Clientes.jsp?cedula="+cedula+"&&direccion="+direccion+"&&email="
+						+email+"&&nombre="+nombre+"&&telefono="+telefono);
+				
+			}else {
+				response.sendRedirect("Clientes.jsp?men=El Cliente no Existe");
+	     	}
+         }
+		
+		if(request.getParameter("actualizar")!=null) {
+			String email,nombre,direccion,telefono;
+			long cedula;
+			cedula = Long.parseLong(request.getParameter("ced"));
+			direccion = request.getParameter("direccion_cliente");
+			email = request.getParameter("email_cliente");
+			nombre = request.getParameter("nombre_cliente");
+			telefono = request.getParameter("telefono_cliente");
+			ClienteDTO clidto_Act = new ClienteDTO(cedula,direccion,email,nombre,telefono);
+		    if(clientedao.ActualizarCliente(clidto_Act)) {
+		      JOptionPane.showMessageDialog(null, "El Cliente se Actualizo Exitosamente!");	
+              response.sendRedirect("Clientes.jsp?men=El Cliente se Actualizo Exitosamente!");  		    
+		    }else {
+		      JOptionPane.showMessageDialog(null, "El Cliente no se Actualizo");	
+	          response.sendRedirect("Clientes.jsp?men=El Cliente no se Actualizo");
+		    }
+		}
+		
+		
+		if(request.getParameter("eliminar")!=null) {
+			long num_cedula;
+			num_cedula = Long.parseLong(request.getParameter("ced"));
+			int op = JOptionPane.showConfirmDialog(null, "Esta seguro de eliminar el Cliente "+num_cedula);
+			if(op==0) {
+				if(clientedao.EliminarCliente(num_cedula)) {
+					response.sendRedirect("Clientes.jsp?men=El Cliente fue Eliminado");
+				}else {
+					response.sendRedirect("Clientes.jsp?men=El Cliente no se Elimino");
+				}
+			}else {
+				response.sendRedirect("Clientes.jsp");
+			}
+		}
+		
+		
+	}
+
+}
---
+++ src/main/java/Controlador/Proveedor.java
@@ -0,0 +1,114 @@
+package Controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+import Modelo.ProveedorDAO;
+import Modelo.ProveedorDTO;
+
+
+
+
+/**
+ * Servlet implementation class Proveedor
+ */
+@WebServlet("/Proveedor")
+public class Proveedor extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Proveedor() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ProveedorDAO proveedorDao = new ProveedorDAO();
+		//pregunto si el proveedor hizo clic en el boton registrar
+		if(request.getParameter("registrar")!=null) {
+			String ciudad,direccion,nombre,telefono;
+			long nit;
+			nit = Long.parseLong(request.getParameter("nitproveedor"));
+			ciudad = request.getParameter("ciudad_proveedor");
+			direccion = request.getParameter("direccion_proveedor");
+			nombre = request.getParameter("nombre_proveedor");
+			telefono= request.getParameter("telefono_proveedor");
+			ProveedorDTO proveedorDto = new ProveedorDTO(nit,ciudad,direccion,nombre,telefono);
+		    if(proveedorDao.Insertarproveedor(proveedorDto)) {
+		      JOptionPane.showMessageDialog(null, "Proveedor Registrado Exitosamente!");	
+              response.sendRedirect("Proveedores.jsp?men=Proveedor Registrado Exitosamente!");  		    
+		    }else {
+		      JOptionPane.showMessageDialog(null, "El Proveedor no se Registro");	
+	          response.sendRedirect("Proveedores.jsp?men=El Proveedor no se Registro");
+		    }
+		
+       	}
+		
+		
+		if(request.getParameter("consultar")!=null){
+			String ciudad,direccion,nombre,telefono;
+			long nit;
+			nit =Long.parseLong(request.getParameter("nitproveedor2"));
+			ProveedorDTO prov = proveedorDao.ConsultarProveedor(nit);
+			if(prov!=null) {
+				nit= prov.getNitproveedor();
+				ciudad = prov.getCiudad_proveedor();
+				direccion = prov.getDireccion_proveedor();
+				nombre =  prov.getNombre_proveedor();
+				telefono = prov.getTelefono_proveedor();
+				
+				//ahora enviar por la url con el response a Proveedores.jsp o la vista jsp
+				response.sendRedirect("Proveedores.jsp?nit="+nit+"&&ciudad="+ciudad+"&&direccion="
+						+direccion+"&&nombre="+nombre+"&&telefono="+telefono);
+				
+			}else {
+				response.sendRedirect("Proveedores.jsp?men=El Proveedor no Existe");
+	     	}
+         }
+		
+		
+		if(request.getParameter("actualizar")!=null) {
+			String ciudad,direccion,nombre,telefono;
+			long nit;
+			nit = Long.parseLong(request.getParameter("nitprov"));
+			ciudad = request.getParameter("ciudad_proveedor");
+			direccion = request.getParameter("direccion_proveedor");
+			nombre = request.getParameter("nombre_proveedor");
+			telefono = request.getParameter("telefono_proveedor");
+			ProveedorDTO provDto_Act = new ProveedorDTO(nit,ciudad,direccion,nombre,telefono);
+		    if(proveedorDao.ActualizarProveedor(provDto_Act)) {
+		      JOptionPane.showMessageDialog(null, "El Proveedor se Actualizo Exitosamente!");	
+              response.sendRedirect("Proveedores.jsp?men=El Proveedor se Actualizo Exitosamente!");  		    
+		    }else {
+		      JOptionPane.showMessageDialog(null, "El Proveedor no se Actualizo");	
+	          response.sendRedirect("Proveedores.jsp?men=El Proveedor no se Actualizo");
+		    }
+		}
+		
+		
+		if(request.getParameter("eliminar")!=null) {
+			long codnit;
+			/*long otronit;
+			otronit=Long.parseLong(request.getParameter("nitprov"));
+			JOptionPane.showMessageDialog(null, otronit);
+			*/
+			codnit = Long.parseLong(request.getParameter("nitprov"));
+			int op = JOptionPane.showConfirmDialog(null, "Esta seguro de eliminar el Proveedor "+codnit);
+			if(op==0) {
+				if(proveedorDao.EliminarProveedor(codnit)) {
+					response.sendRedirect("Proveedores.jsp?men=El Proveedor fue Eliminado");
+				}else {
+					response.sendRedirect("Proveedores.jsp?men=El Proveedor no se Elimino");
+				}
+			}else {
+				response.sendRedirect("Proveedores.jsp");
+			}
+		}		
+		
+    }
+}
