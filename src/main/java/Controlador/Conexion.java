ffd01912610b8bf2f959c7f581a319d720e97851
Steve <Steve@compu-de-Steve>
2021-09-25 08:33:29 -0500

    cambio nombre de base de datos SQL

--- src/main/java/Controlador/Conexion.java
+++ src/main/java/Controlador/Conexion.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
 
-	private String bd="idealbeuty";
+	private String bd="idealbeauty";
 	private String url="jdbc:mysql://localhost:3306/"+bd;
 	private String user="root";
 	private String pass="";
