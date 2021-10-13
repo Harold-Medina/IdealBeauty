4835b5d58440eb0e344ed86ed055249e6fe0cf5d
Steve <Steve@compu-de-Steve>
2021-10-13 17:13:45 -0500

    base de datos maria funcionando

--- src/main/java/Controlador/Conexion.java
+++ src/main/java/Controlador/Conexion.java
@@ -6,15 +6,15 @@ import javax.swing.JOptionPane;
 
 public class Conexion {
 
-	private String bd="idealbeauty";
-	private String url="jdbc:mysql://localhost:3306/"+bd;
-	private String user="root";
-	private String pass="";
+	private String bd="Grupo31_Equipo_2";
+	private String url="jdbc:mariadb://prestamosvf.czo3ixoe3xoe.us-east-1.rds.amazonaws.com/"+bd;
+	private String user="admin";
+	private String pass="Ciclo3_admi123";
 	Connection conec=null;
 	
 	public Connection Conectar() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			conec= DriverManager.getConnection(url,user,pass);
 			///JOptionPane.showMessageDialog(null,"Conexion ok..");
 			
